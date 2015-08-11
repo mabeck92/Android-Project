bb8cff38d7ba99e094385fdd5c97bf95192a3e93
mabeck92 <marcus-beck92@gmx.de>
2015-08-11 17:40:27 +0200

    first Layout
    implemented first layout

---
+++ ParseStarterProject/src/com/parse/starter/JSONtoObjectConverter.java
@@ -0,0 +1,15 @@
+package com.parse.starter;
+
+/**
+ * Created by Marcus on 11.08.2015.
+ */
+public class JSONtoObjectConverter{
+    private static final String QUESTION ="question";
+    private static final String OPTION_A ="option_A";
+    private static final String OPTION_B ="option_B";
+    private static final String OPTION_C ="option_C";
+    private static final String OPTION_D ="option_D";
+    private static final String CORRECT_ANSWER ="correct_answer";
+
+
+}
